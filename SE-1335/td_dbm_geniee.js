206849239136198057004b11c5023bac3de89ad7
Yoshihiro Shimomura <adjmpwgt+github@gmail.com>
2018-07-24 13:20:01 +0900

    [SE-1335] Commonization of sync functions for google and geniee

--- SE-1335/td_dbm_geniee.js
+++ SE-1335/td_dbm_geniee.js
@@ -2,32 +2,28 @@
 
 var createImage = function(url) {
   var img = document.createElement('img');
-  img.src = (('https:' == document.location.protocol) ? 'https://' : 'http://') + url;
-  img.width=1;img.height=1;img.style.display='none';
+  img.src = url;img.width=1;img.height=1;img.style.display='none';
   document.body.appendChild(img);
 };
 
-var syncCallback1 = function() {
-  var td_client_id = td.client.track.uuid;
+var syncCallback1 = function(client_id,domain) {
   var region = td.client.host.split('.')[0];
   var account = (region === 'in' ? 'aws-':(region === 'tokyo' ? 'aws-tokyo-':'idcf-')) + td.client.writeKey.split('/')[0];
-  var dbm_url = 'cm.g.doubleclick.net/pixel?google_nid=treasuredata_dmp&google_cm&td_write_key=8151/fcd628065149d648b80f11448b4083528c0d8a91&td_global_id=td_global_id' + '&td_client_id=' + td_client_id + '&account=' + account + '&td_host=' + document.location.host;
+  var dbm_url = 'https://cm.g.doubleclick.net/pixel?google_nid=treasuredata_dmp&google_cm&td_write_key=8151/fcd628065149d648b80f11448b4083528c0d8a91&td_global_id=td_global_id' + '&td_client_id=' + client_id + '&account=' + account + '&td_host=' + domain;
   createImage(dbm_url);
 };
 
-var syncCallback2 = function() {
-  var client_id = encodeURIComponent(td.getCookie('_td'));
-  var domain = encodeURIComponent(document.location.host);
+var syncCallback2 = function(client_id,domain) {
   var src_url = 'https://in.treasuredata.com/postback/v3/event/test_db/tdid_gid_mapping?td_format=pixel&td_write_key=9245/36083a629905e1319ef47f071c5d06ff870ec9a4&td_global_id=td_global_id&gid=${GID_URLENCODE}' + `&td_client_id=${client_id}` + `&domain=${domain}`;
   var redirect_url = "https://cs.gssprt.jp/yie/ld/rd?dest=" + encodeURIComponent(src_url);
-  var img_tag = document.createElement('img');
-  img_tag.src = redirect_url;
-  document.body.appendChild(img_tag);
+  createImage(redirect_url);
 };
 
 var successCallback = function() {
-  syncCallback1();
-  syncCallback2();
+  var client_id = encodeURIComponent(td.getCookie('_td'));
+  var domain = encodeURIComponent(document.location.host);
+  syncCallback1(client_id,domain);
+  syncCallback2(client_id,domain);
 };
 
 var td = new Treasure({
