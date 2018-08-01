a3b67dacea0451ae80ed42f88a5043e98b262a12
Yoshihiro Shimomura <adjmpwgt+github@gmail.com>
2018-08-01 11:51:44 +0900

    add test code 2

--- SE-1356/dbm_td_en_20180801.js
+++ SE-1356/dbm_td_en_20180801.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 !function(t,e){if(void 0===e[t]){e[t]=function(){e[t].clients.push(this),this._init=[Array.prototype.slice.call(arguments)]},e[t].clients=[];for(var r=function(t){return function(){return this["_"+t]=this["_"+t]||[],this["_"+t].push(Array.prototype.slice.call(arguments)),this}},s=["addRecord","set ","trackEvent","trackPageview","trackClicks","ready","fetchGlobalID","fetchUserSegments"],a=0;a<s.length;a++){var c=s[a];e[t].prototype[c]=r(c)}var n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=("https:"===document.location.protocol?"https:":"http:")+"//cdn.treasuredata.com/sdk/1.9.2/td.min.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("Treasure",this);
 
 var createImage = function(url) {
@@ -26,4 +25,3 @@ var td = new Treasure({
 //td.set('00_basic', {member_id: {{MemberID}} });
 // Track pageview information to table
 td.trackPageview('se_1356',sync_callback);
-</script>
