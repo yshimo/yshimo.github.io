2de75be35bf5271adb756c36d04ce33bb4d67cc1
Yoshihiro Shimomura <adjmpwgt+github@gmail.com>
2018-08-21 12:16:41 +0900

    SE-1406 test

--- SE-1406/td_dbm_20180821_1.js
+++ SE-1406/td_dbm_20180821_1.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
  !function(t,e){if(void 0===e[t]){e[t]=function(){e[t].clients.push(this),this._init=[Array.prototype.slice.call(arguments)]},e[t].clients=[];for(var r=function(t){return function(){return this["_"+t]=this["_"+t]||[],this["_"+t].push(Array.prototype.slice.call(arguments)),this}},s=["addRecord","set","trackEvent","trackPageview","trackClicks","ready","fetchGlobalID","fetchUserSegments"],a=0;a<s.length;a++){var c=s[a];e[t].prototype[c]=r(c)}var n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=("https:"===document.location.protocol?"https:":"http:")+"//cdn.treasuredata.com/sdk/1.9.2/td.min.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("Treasure",this);
 </script>
 <script type="text/javascript">
@@ -34,4 +33,3 @@
   //  ism_lastpage:'no'
   //});
   td.trackPageview('se_1406',sync_callback);
-</script>
